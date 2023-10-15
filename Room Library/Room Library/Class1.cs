db2807b7f1e5f841f2014970cfd53625be3eaf78
stal_lker <138815471+Stallker202@users.noreply.github.com>
2023-10-15 20:33:59 +0300

    Add files via upload

---
+++ Room Library/Room Library/Class1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Room_Library
+{
+    public class Room
+    {
+        private double roomLength;
+
+        private double roomWidth;
+
+        public double RoomLength
+        {
+            get
+            {
+                return roomLength;
+            }
+            set
+            {
+                roomLength = value;
+            }
+        }
+
+        public double RoomWidth
+        {
+            get
+            {
+                return roomWidth;
+            }
+            set
+            {
+                roomWidth = value;
+            }
+        }
+
+        public double RoomPerimetr()
+        {
+            return 2.0 * (roomLength + roomWidth);
+        }
+
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+
+        public double PersonArea(int np)
+        {
+            return RoomArea() / (double)np;
+        }
+
+        public virtual string Info()
+        {
+            return "Комната площадью " + RoomArea() + "кв.м";
+        }
+    }
+
+    public class LivingRoom : Room
+        {
+            private int numWin;
+
+            public int NumWin
+            {
+                get
+                {
+                    return numWin;
+                }
+                set
+                {
+                    numWin = value;
+                }
+            }
+
+            public override string Info()
+            {
+                return "Жилая комната площадью " + RoomArea() + " кв.м, с " + numWin + " окнами";
+            }
+
+    }
+
+    public class Office : Room
+    {
+        private int numSockets;
+
+        public int NumSockets
+        {
+            get
+            {
+                return numSockets;
+            }
+            set
+            {
+                numSockets = value;
+            }
+        }
+
+        public int NumWorkplaces()
+        {
+            int val = Convert.ToInt32(Math.Truncate(RoomArea() / 4.5));
+            return Math.Min(numSockets, val);
+        }
+
+        public override string Info()
+        {
+            return "Офис на " + NumWorkplaces() + " рабочих мест";
+        }
+    }
+}
---
+++ RoomExamplePro_Gerasimov/RoomExamplePro_Gerasimov/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Room_Library;
+
+namespace RoomExamplePro_Gerasimov
+{
+    public partial class MainWindow : Window
+    {
+        List<Room> lstRooms = new List<Room>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BAddRoom_Click(object sender, RoutedEventArgs e)
+        {
+            Room room = new Room();
+            room.RoomLength = Convert.ToDouble(TBLengthR.Text);
+            room.RoomWidth = Convert.ToDouble(TBWidthR.Text);
+            lstRooms.Add(room);
+        }
+
+        private void BAddLivingRoom_Click(object sender, RoutedEventArgs e)
+        {
+            LivingRoom livingRoom = new LivingRoom();
+            livingRoom.RoomLength = Convert.ToDouble(TBLengthL.Text);
+            livingRoom.RoomWidth = Convert.ToDouble(TBWidthL.Text);
+            livingRoom.NumWin = Convert.ToInt32(TBNumW.Text);
+            lstRooms.Add(livingRoom); 
+        }
+
+        private void BAddOffice_Click(object sender, RoutedEventArgs e)
+        {
+            Office office = new Office();
+            office.RoomLength = Convert.ToDouble(TBLengthO.Text);
+            office.RoomWidth = Convert.ToDouble(TBWidthO.Text);
+            office.NumSockets = Convert.ToInt32(TBNumS.Text);
+            lstRooms.Add(office); 
+        }
+
+        private void BGetList_Click(object sender, RoutedEventArgs e)
+        {
+            ListRooms.Content = "";
+            foreach (Room r in lstRooms)
+                ListRooms.Content += r.Info() + "\n";
+        }
+    }
+}
